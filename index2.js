e32b404360f6735d61d8ac136304e167074a24c1
Vishakha Chaursiya <vishakhachaurasiya2003@gmail.com>
2025-11-07 12:30:04 +0530

    first commit

---
+++ index2.js
@@ -0,0 +1,18 @@
+function inputData(val){
+    document.getElementById("screen").value +=val;
+}
+
+function clearscreen(){
+    document.getElementById("screen").value="";
+}
+function finalvalue(){
+let screenval=document.getElementById("screen").value;
+let result=eval(screenval);
+document.getElementById("screen").value=result;
+
+}
+function singlclear(){
+let screenval=document.getElementById("screen").value;
+document.getElementById("screen").value=screenval.slice(0,-1);
+
+}
